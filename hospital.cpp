dd5efb9a2812055dd1b4a0ade102826c3d474f7e
Krishna Malani <145795573+Krish7Hack@users.noreply.github.com>
2024-09-30 15:38:30 +0530

    Add files via upload

---
+++ hospital.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Stores the data of Hospital.
+class Hospital{
+    public:
+        string H_name;
+        sting location;
+        int available_beds;
+        float rating;
+        string contact;
+        string doctor_name;
+        int price;
+};
